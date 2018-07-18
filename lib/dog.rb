3234acfec0627f8dc0bfc556cb4a32275b2677e4
Ramona M <r1mari@yahoo.com>
2018-07-18 06:09:31 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,6 @@
+class Dog 
+end
+fido= Dog.new 
+fido
+  snoopy=Dog.new
+  lassie=Dog.new
---
+++ lib/person.rb
@@ -0,0 +1,9 @@
+class Person
+end
+
+
+fido= Person.new
+
+adele_goldberg = Person.new
+alan_kay = Person.new
+
